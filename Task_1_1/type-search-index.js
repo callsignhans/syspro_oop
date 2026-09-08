2a24c5e0a1c87f3cd788d85ff5c7027c976df059
callsignhans <callsignhans@users.noreply.github.com>
2026-09-08 10:05:39 +0000

    Deploying to gh-pages from @ callsignhans/syspro_oop@1bdcb7c1772fbf9fcfe36a305b8198422d8ec8c9 🚀

---
+++ Task_1_1/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"BinarySortHeap"},{"p":"<Unnamed>","l":"HeapSort"},{"p":"org.example","l":"Main"}];updateSearchResults();
