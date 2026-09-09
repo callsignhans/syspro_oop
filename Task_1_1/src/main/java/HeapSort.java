fe6ed91dda3d619d5a8224fafe76a1a31878800a
callsignhans <genrihmuller812@gmail.com>
2026-09-09 15:48:27 +0700

    add shell file

--- Task_1_1/src/main/java/HeapSort.java
+++ Task_1_1/src/main/java/HeapSort.java
@@ -1,6 +1,16 @@
 import java.util.ArrayList;
 
+/**
+ * Class with static method heapsort.
+ */
 public class  HeapSort {
+
+    /**
+     * Sorts the provided array using heapsort
+     * @param notSortArray Unsorted array
+     * @return Sorted array
+     * @param <T> Type elements in array
+     */
     public static <T extends Comparable<? super T>> ArrayList<T> HeapSortCall(T[] notSortArray) {
         var heap = new BinarySortHeap<>(notSortArray);
         var result = new ArrayList<T>();
