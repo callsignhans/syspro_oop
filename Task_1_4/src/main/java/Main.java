ad62fb11da3f0377d341d133468426bc288f9504
callsignhans <genrihmuller812@gmail.com>
2026-09-26 00:24:47 +0700

    first commit in task

---
+++ Task_1_4/src/main/java/Main.java
@@ -0,0 +1,15 @@
+
+
+/**
+ * Start point class.
+ */
+public class Main {
+
+    /**
+     * Start point method.
+     * @param args argument command line
+     */
+    public static void main(String[] args) {
+        int i = 1;
+    }
+}
